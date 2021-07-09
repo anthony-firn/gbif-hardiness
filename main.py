3d5af6ac8b5d348f419a069589b6e70b006ed239
anthony4tner <anthony4tner2@gmail.com>
2021-07-08 23:34:24 -0700

    Got Data

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pygbif import maps, utils
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    out = maps.map(taxonKey=212, year=1998, bin="hex", hexPerTile=300, style="classic-noborder.poly")
+    out.response
+    out.path
+    out.img
+    out.plot()
+    x = 'POLYGON((144.6 13.2, 144.6 13.6, 144.9 13.6, 144.9 13.2, 144.6 13.2))'
+    print(utils.wkt_rewind(x))
 
 
 # Press the green button in the gutter to run the script.
